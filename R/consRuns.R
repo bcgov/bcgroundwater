4571d7b3e248e405d1229b9479da86307d98a4a6
Andy Teucher <andy.teucher@gov.bc.ca>
2014-08-22 14:28:29 -0700

    use occur funtion to match up vector position

--- R/consRuns.R
+++ R/consRuns.R
@@ -19,76 +19,59 @@
 #'}
 consRuns <- function(x, val, head=0.2, tail=0.8, n_consec)  {
   
-  # The function body
-  runs <- unclass(rle(x))
-  runs$end <- cumsum(runs$lengths)
-  runs$beginning <- runs$end - runs$lengths
-  runs <- as.data.frame(runs, stringsAsFactors=FALSE)
-  runs <- runs[runs$values == val & runs$lengths >= n_consec,]
+  x_orig <- x
   
-  # Find where runs occur in head
-  head_remove <- suppressWarnings(
-    max(runs$end[length(x) * head - runs$beginning >= n_consec])
-  )
-  if (is.infinite(head_remove)) head_remove <- 1
-  if (x[head_remove] == val) head_remove <- head_remove + 1
-  
-  # Find where runs occur in tail:
-  tail_remove <- suppressWarnings(
-    min(runs$beginning[runs$end - length(x) * tail >= n_consec])
-  )
-  
-  if (is.infinite(tail_remove)) tail_remove <- length(x)
-  
-  # The recursive part. TODO: iterate head_remove and tail_remove
-  if (head_remove == 1 && tail_remove == length(x)) {
-    return(x[head_remove:tail_remove])
-    #return(list(start=head_remove, end=tail_remove))
-  } else {
-    consRuns(x[head_remove:tail_remove], val, head, tail, n_consec)
+  # occur function from here:
+  # http://r.789695.n4.nabble.com/matching-a-sequence-in-a-vector-td4389523.html
+  occur <- function(patrn, exmpl)
+  {
+    m <- length(patrn)
+    n <- length(exmpl)
+    candidate <- seq.int(length=n-m+1)
+    for (i in seq.int(length=m)) {
+      candidate <- candidate[patrn[i] == exmpl[candidate + i - 1]]
+    }
+    candidate
   }
   
-}
-
-# http://stackoverflow.com/questions/16480722/recursive-functions-and-global-vs-local-variables
-
-consRuns_r <- function(x, val, head=0.1, tail=0.9, n_consec)  {
-  
-  runs <- unclass(rle(x))
-  runs$end <- cumsum(runs$lengths)
-  runs$beginning <- runs$end - runs$lengths
-  runs <- as.data.frame(runs, stringsAsFactors=FALSE)
-  cons_runs <- runs[runs$values == val & runs$lengths >= n_consec,]
-  
-  repeat {
-    # Find where runs occur in head
-    # NOt working on subsequent iterations because runs not updated, so length 
-    # is of original ... need to keep track of updated length
-    in_head <- cons_runs[sum(runs$lengths) * head - 
-                               cons_runs$beginning >= n_consec,]
+  wrap <- function(x, val, head, tail, n_consec) {
     
+    # The function body
+    runs <- unclass(rle(x))
+    runs$end <- cumsum(runs$lengths)
+    runs$beginning <- runs$end - runs$lengths
+    runs <- as.data.frame(runs, stringsAsFactors=FALSE)
+    runs <- runs[runs$values == val & runs$lengths >= n_consec,]
+    
+    # Find where runs occur in head
     head_remove <- suppressWarnings(
-      max(in_head$end)
+      max(runs$end[length(x) * head - runs$beginning >= n_consec])
     )
     if (is.infinite(head_remove)) head_remove <- 1
     if (x[head_remove] == val) head_remove <- head_remove + 1
     
     # Find where runs occur in tail:
-    in_tail <- cons_runs[cons_runs$end - sum(runs$lengths) * 
-                                     tail >= n_consec,]
     tail_remove <- suppressWarnings(
-      min(in_tail$begining)
+      min(runs$beginning[runs$end - length(x) * tail >= n_consec])
     )
     
-    if (is.infinite(tail_remove)) tail_remove <- sum(runs$lengths)
+    if (is.infinite(tail_remove)) tail_remove <- length(x)
     
+    # The recursive part. TODO: iterate head_remove and tail_remove
     if (head_remove == 1 && tail_remove == length(x)) {
-      break
+      #return(x[head_remove:tail_remove])
+      start <- occur(x[head_remove:tail_remove], x_orig)
+      end <- length(x[head_remove:tail_remove]) + start - 1
+      return(list(start = start, end = end))
+    } else {
+      wrap(x[head_remove:tail_remove], val, head, tail, n_consec)
     }
     
-    if (head_remove != 1) cons_runs <- cons_runs[-1,]
-    #return(list(start=head_remove, end=tail_remove))
   }
-  x <- x[head_remove:tail_remove]
-  x
+  
+  wrap(x, val, head, tail, n_consec)
+  
 }
+
+# http://stackoverflow.com/questions/16480722/recursive-functions-and-global-vs-local-variables
+                                                                                                          
--- tests/testthat/test-cumRuns.R
+++ tests/testthat/test-cumRuns.R
@@ -2,29 +2,29 @@
 set.seed(20)
 x <- rnorm(200)
 x[2:10] <- 0
-cumRuns(x, val = 0, n_consec = 5)
+y <- consRuns(x, val = 0, n_consec = 5)
 
 # One set of missing vals in tail
 set.seed(20)
 x <- rnorm(200)
 x[190:198] <- 0
-cumRuns(x, val = 0, n_consec = 5)
+consRuns(x, val = 0, n_consec = 5, head = 0.1, tail = 0.9)
 
 # Two sets in head
 set.seed(20)
 x <- rnorm(200)
-x[c(2:8, 20:30)] <- 0 # check that these would be evaluated in two loops
-cumRuns(x, val = 0, n_consec = 5)
+x[c(2:8, 25:35)] <- 0 # check that these would be evaluated in two loops
+consRuns(x, val = 0, n_consec = 5, head = 0.1, tail = 0.9)
 
 # Two sets in tail
 set.seed(20)
 x <- rnorm(200)
 x[c(185:190, 194:199)] <- 0
-cumRuns(x, val = 0, n_consec = 5)
+consRuns(x, val = 0, n_consec = 5)
 
 # Two sets in head and tail
 set.seed(20)
 x <- rnorm(100000)
 x[c(10:20, 2000:2010, 10000:10099,90010:90020,95000:96000)] <- 0
-foo <- cumRuns(x, val = 0, n_consec = 5)
+foo <- consRuns(x, val = 0, n_consec = 5)
 
