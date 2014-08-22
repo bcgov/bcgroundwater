3cb8948482a22f31565f95eca881c16dfc4e5c3b
Andy Teucher <andy.teucher@gov.bc.ca>
2014-08-22 11:11:59 -0700

    changes to loop

--- R/tmp.R
+++ R/tmp.R
@@ -5,18 +5,25 @@ x[c(2:8, 21:27, 170:179, 194:199)] <- 0
 val <- 0
 n_consec = 5
 head = 0.1
+tail = 0.9
 
 runs <- unclass(rle(x))
 runs <- as.data.frame(runs, stringsAsFactors=FALSE)
 runs$end <- cumsum(runs$lengths)
-runs$beginning <- runs$end - runs$lengths
+runs$beginning <- runs$end - runs$lengths + 1
 val_runs <- runs$values == val & runs$lengths >= n_consec
+h <- 1
+t <- length(x)
 
 for (i in which(val_runs)) {
   run <- runs[i,]
-  try(
-    if (run$beginning <= sum(runs$lengths) * head) {
-      runs <- runs[i:length(runs),]
-    }
-  )
+  
+  if (run$beginning + n_consec <= length(x[h:t]) * head) {
+    h <- run$end + 1
+  }
+  
+  if (run$end - n_consec >= length(x[h:t]) * tail) {
+    t <- run$beginning - 1
+  }
+  
 }
