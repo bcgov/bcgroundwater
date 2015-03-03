52f190a74283991d8e600a6d8c08a96182204646
Andy Teucher <andy.teucher@gov.bc.ca>
2015-03-03 14:12:18 -0800

    Document package

---
+++ R/bcgroundwater.R
@@ -0,0 +1,10 @@
+#' bcgroundwater: Analyse B.C. groundwater level data.
+#'
+#' Set of functions for downloading, analysing, and visualising
+#' groundwater level data from the BC Ministry of Environment groundwater
+#' observation well network.
+#'
+#' @docType package
+#' @author Andy Teucher \email{andy.teucher@@gov.bc.ca}
+#' @name bcgroundwater
+NULL
