7be8ff45de95f078abcac762d52476ae586f244c
Stefanie LaZerte <steffi@steffi.ca>
2017-11-21 16:27:46 -0600

    Add tests to confirm monthlyValues() and gwlMonthlyPlot() not broken with get_gwl()

---
+++ tests/testthat/test_monthlyValues.R
@@ -0,0 +1,10 @@
+context("monthlyValues and gwlMonthlyPlot")
+
+test_that("monthlyValues returns data", {
+  g <- get_gwl(well = 309)
+  expect_silent(m <- monthlyValues(g))
+  expect_gt(nrow(m), 0)
+  
+  expect_silent(p <- gwlMonthlyPlot(m, last12 = TRUE))
+  expect_is(p, "ggplot")
+})
