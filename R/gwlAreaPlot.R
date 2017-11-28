fe86f7b90f0794002c2f90a14abe40aeb0c8dfdc
Stefanie LaZerte <steffi@steffi.ca>
2017-11-28 15:06:32 -0600

    Tweak formating, rewrite without global variables

--- R/gwlAreaPlot.R
+++ R/gwlAreaPlot.R
@@ -14,7 +14,7 @@
 #'
 #' An area plot (hydrograph) of groundwater levels, with trend line of a 
 #' given slope and intercept, optionally with interpolated values shown.
-#' @import ggplot2
+
 #' @importFrom scales date_breaks date_format
 #' @param  dataframe a dataframe with well level monthly time series and the 
 #'         following columns: Date, med_GWL, nReadings
@@ -25,13 +25,18 @@
 #' @param  showInterpolated (logical) show the points where missing values in the 
 #'         time series were interpolated
 #' @param  save Save the graph to file?
-#' @param  path Where to save the graph if save=TRUE
+#' @param  path Where to save the graph if \code{save = TRUE}
 #' @param  mkperiod the period (monthly or annual) the Mann-Kendall test was 
 #'         performed on
 #' @param  opts other options to pass to ggplot2
-#' @export
+#' 
 #' @return A ggplot2 object
-gwlAreaPlot <- function(dataframe, trend, intercept, state, sig, showInterpolated=FALSE, save=FALSE, path="./", mkperiod="monthly", opts=NULL) {
+#' 
+#' @import ggplot2
+#' @export
+gwlAreaPlot <- function(dataframe, trend, intercept, state, sig, 
+                        showInterpolated = FALSE, save = FALSE, 
+                        path = "./", mkperiod = "monthly", opts = NULL) {
   
   if (showInterpolated) {
     df <- dataframe
@@ -43,8 +48,10 @@ gwlAreaPlot <- function(dataframe, trend, intercept, state, sig, showInterpolate
     df <- dataframe[dataframe$nReadings > 0,]
   }
   
-  minDate <- min(as.Date(df$Date))
-  maxDate <- max(as.Date(df$Date))
+  df$Date <- as.Date(df$Date)
+  
+  minDate <- min(df$Date)
+  maxDate <- max(df$Date)
   
   WellNum <- df$Well_Num[1]
   
@@ -55,52 +62,57 @@ gwlAreaPlot <- function(dataframe, trend, intercept, state, sig, showInterpolate
     slope <- -(trend/12/365)
   }
   
-  trendpre <- ifelse(slope > 0,"Trend: +", "Trend: ")
-  trendprintval <- paste0(format(slope*365,digits=2, nsmall=2
-                                 , scientific=FALSE),"m/year")
+  trendpre <- ifelse(slope > 0, "Trend: +", "Trend: ")
+  trendprintval <- paste0(format(slope * 365, digits = 2, nsmall = 2,
+                                 scientific = FALSE), "m/year")
   
   trendprint <- paste0(trendpre, trendprintval)
   
-  int.well <- intercept + slope*as.numeric(minDate)
+  int.well <- intercept + slope * as.numeric(minDate)
   
-  maxgwl <- max(df$med_GWL, na.rm=TRUE)
-  mingwl <- min(df$med_GWL, na.rm=TRUE)
-  gwlrange <- maxgwl-mingwl
-  midgwl <- (maxgwl+mingwl)/2
-  lims <- c(midgwl+gwlrange, midgwl-gwlrange)
+  maxgwl <- max(df$med_GWL, na.rm = TRUE)
+  mingwl <- min(df$med_GWL, na.rm = TRUE)
+  gwlrange <- maxgwl - mingwl
+  midgwl <- (maxgwl + mingwl)/2
+  lims <- c(midgwl + gwlrange, midgwl - gwlrange)
   
-  plot.area <- ggplot(df, aes(x=as.Date(Date))) + 
-    geom_ribbon(aes(ymin=med_GWL, ymax=max(lims[1],max(med_GWL, na.rm=TRUE)+5)
-                    , fill="Groundwater Level"), alpha=0.3) + 
-    geom_abline(aes(intercept = intercept, slope = slope, colour='LTT'), 
-                data = data.frame(intercept=-int.well, slope=slope), size=1) + 
-    annotate(geom="text", x=minDate+as.numeric(maxDate-minDate)/50
-             , y=lims[2], hjust=0, vjust=1, colour="grey50", size=3
-             , label=paste0(trendprint, "        Significance: "
-                            , format(sig, digits=2, nsmall=3
-                                     , scientific=3)
-                            , "        State: ", state)) + 
-    labs(title="Groundwater levels and long-term trend", x="Date"
-         , y="Depth below ground (m)") + 
-    theme(panel.background=element_rect(fill="white")
-          , line=element_line(colour="grey50")
-          , text=element_text(colour="grey50")
-          , panel.grid.major.x=element_blank()
-          , panel.grid.minor.x=element_blank()
-          , legend.position="top", legend.box="horizontal") + 
-    scale_y_reverse(expand=c(0,0)) + coord_cartesian(ylim=lims) + 
-    scale_x_date(labels=date_format("%Y"), breaks=date_breaks("2 years")
-                 , expand=c(0,0)) + 
-    scale_fill_manual(name='', values=c('Groundwater Level' = "#1E90FF"))
+  df$max_lims <- max(lims[1], max(df$med_GWL, na.rm = TRUE) + 5)
   
-  vals <- c(LTT='orange', Interp='red')
+  plot.area <- ggplot(df, aes_string(x = "Date")) + 
+    geom_ribbon(aes_string(ymin = "med_GWL", 
+                           ymax = "max_lims",
+                           fill = "'Groundwater Level'"), alpha = 0.3) + 
+    geom_abline(aes_string(intercept = "intercept", slope = "slope", colour = "'LTT'"), 
+                data = data.frame(intercept = -int.well, slope = slope), size = 1) + 
+    annotate(geom = "text", 
+             x = minDate + as.numeric(maxDate - minDate)/50, 
+             y = lims[2], hjust = 0, vjust = 1, colour = "grey50", size = 3,
+             label = paste0(trendprint, "        Significance: ",
+                            format(sig, digits = 2, nsmall = 3, scientific = 3),
+                            "        State: ", state)) + 
+    labs(title = "Groundwater levels and long-term trend", x = "Date",
+         y = "Depth below ground (m)") + 
+    theme(panel.background = element_rect(fill = "white"),
+          line = element_line(colour = "grey50"),
+          text = element_text(colour = "grey50"),
+          panel.grid.major.x = element_blank(),
+          panel.grid.minor.x = element_blank(),
+          legend.position = "top", legend.box =  "horizontal",
+          plot.title = element_text(hjust = 0.5)) + 
+    scale_y_reverse(expand = c(0,0)) + coord_cartesian(ylim = lims) + 
+    scale_x_date(labels = date_format("%Y"), breaks = date_breaks("2 years"),
+                 expand = c(0,0)) + 
+    scale_fill_manual(name = '', values = c('Groundwater Level' = "#1E90FF"))
+   
+  vals <- c(LTT = 'orange', Interp = 'red')
   labs <- c('Long-term trend', 'Interpolated (missing) values')
-  override_list <- list(colour = c("orange", "red"), shape=c(NA, 16), linetype=c(1, 0))
+  override_list <- list(colour = c("orange", "red"), shape = c(NA, 16), linetype = c(1, 0))
   
   if (showInterpolated) {
-    plot.area <- plot.area + geom_point(data=df[df$nReadings==0,]
-                                        , aes(y=med_GWL, colour='Interp')
-                                        , size=1)
+    plot.area <- plot.area + 
+      geom_point(data = df[df$nReadings == 0,],
+                 aes_string(y = "med_GWL", colour = "'Interp'"),
+                 size = 1)
   } else {
     vals <- vals[1]
     labs <- labs[1]
@@ -108,14 +120,14 @@ gwlAreaPlot <- function(dataframe, trend, intercept, state, sig, showInterpolate
   }
   
   plot.area <- plot.area +
-    scale_colour_manual(name = '', values=vals, labels = labs,
+    scale_colour_manual(name = '', values = vals, labels = labs,
                         guide = guide_legend(override.aes = override_list)) +
     
     opts
   
   if (save) {
-    ggsave(filename=paste0(path, "trend_chart_well_", WellNum, ".pdf")
-           , plot=plot.area)
+    ggsave(filename = paste0(path, "trend_chart_well_", WellNum, ".pdf"),
+           plot = plot.area)
   }
   
   return(plot.area)
