9adbcc9a0bfb1cc7e4687c0388e14351b9ec5559
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2018-06-13 11:36:06 -0700

    change arg defaults

--- R/trimConsRuns.R
+++ R/trimConsRuns.R
@@ -16,11 +16,11 @@
 #' beginning and end of a vector.
 #' @param  x The vector
 #' @param  val The value for which to find consecutive runs
-#' @param  head (Proportion between 0 and 1, default = 0.2) The beginning portion of 
+#' @param  head (Proportion between 0 and 1, default = 0.1) The beginning portion of 
 #'         the vector in which to look for consecutive missing values
-#' @param  tail (Proportion between 0 and 1, default = 0.8) The end portion of 
+#' @param  tail (Proportion between 0 and 1, default = 0.9) The end portion of 
 #'         the vector in which to look for consecutive missing values
-#' @param  n_consec The number of consecutive values which constitutes a 'long' run
+#' @param  n_consec The number of consecutive values which constitutes a 'long' run (default = 4)
 #' @export
 #' @return A list of start and end points of a vector to remove heavily weighted
 #'         heads and tails containing continuous runs of a value (likely denoting missing)
@@ -29,7 +29,7 @@
 #'  trunc <- trimConsRuns(x, 1, n_consec=4)
 #'  trunc_x <- x[trunc$start:trunc$end]
 #'}
-trimConsRuns <- function(x, val, head=0.2, tail=0.8, n_consec)  {
+trimConsRuns <- function(x, val, head=0.1, tail=0.9, n_consec = 4)  {
   
   x_orig <- x
   
