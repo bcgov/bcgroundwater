3b06913cfc2d23dd3be83c73f7a17bd00b387503
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2018-06-15 14:13:28 -0700

    remove date, build vignette

--- inst/doc/bcgroundwater.R
+++ inst/doc/bcgroundwater.R
@@ -1,34 +1,55 @@
 ## ----set-options, echo=FALSE---------------------------------------------
 knitr::opts_chunk$set(fig.width = 6, fig.height = 4)
 
+## ---- eval=FALSE---------------------------------------------------------
+#  install.packages("remotes")
+
+## ---- eval=FALSE---------------------------------------------------------
+#  library("remotes")
+#  install_github("bcgov/bcgroundwater")
+
 ## ------------------------------------------------------------------------
 library(bcgroundwater)
 
-## ------------------------------------------------------------------------
-daily_data <- get_gwl(well = 309, which = "daily")
+## ---- message=FALSE, warning=FALSE---------------------------------------
+daily_data <- get_gwl(wells = 309, which = "daily")
 head(daily_data)
 
-## ------------------------------------------------------------------------
-recent_data <- get_gwl(well = 309, which = "recent")
+## ---- message=FALSE, warning=FALSE---------------------------------------
+recent_data <- get_gwl(wells = 309, which = "recent")
 head(recent_data)
 
-## ------------------------------------------------------------------------
-data <- get_gwl(well = 309)
+## ---- message=FALSE, warning=FALSE---------------------------------------
+data <- get_gwl(wells = 309)
 head(data)
 
-## ------------------------------------------------------------------------
-monthly_data <- monthlyValues(data)
+## ---- message=FALSE, warning=FALSE---------------------------------------
+monthly_data <- monthly_values(data)
 head(monthly_data)
 
-## ------------------------------------------------------------------------
-monthlyplot <- gwlMonthlyPlot(monthly_data, last12 = TRUE)
+## ---- message=FALSE, warning=FALSE, fig.align='center'-------------------
+monthlyplot <- gwl_monthly_plot(monthly_data, last12 = TRUE)
 plot(monthlyplot)
 
-## ---- fig.width=6, fig.height=4------------------------------------------
-full_monthly_data <- makeWellTS(monthly_data)
-head(monthly_data)
+## ---- fig.width=6, fig.height=4, message=FALSE, warning=FALSE------------
+full_monthly_data <- make_well_ts(monthly_data)
+head(full_monthly_data)
 
-## ------------------------------------------------------------------------
+## ---- message=FALSE, warning=FALSE---------------------------------------
+library(dplyr)
+
+time_series_attr <- full_monthly_data %>%
+  group_by(Well_Num) %>%
+  summarise(dataStart = as.Date(min(Date)), 
+            dataEnd = as.Date(max(Date)), 
+            dataYears = as.numeric(dataEnd - dataStart) / 365, 
+            nObs = n(), 
+            nMissing = length(med_GWL[nReadings == 0]), 
+            percent_missing = round(nMissing/nObs*100, 1)) %>% 
+  select(Well_Num, dataYears, nMissing, percent_missing) 
+time_series_attr
+
+## ---- message=FALSE, warning=FALSE---------------------------------------
 library(dplyr)
 
 annual_data <- full_monthly_data %>%
@@ -39,15 +60,25 @@ annual_data <- full_monthly_data %>%
             SD = sd(med_GWL),
             med_GWL = median(med_GWL),
             q95_GWL = quantile(med_GWL, 0.95))
+head(annual_data)
 
-## ------------------------------------------------------------------------
-trends <- gwlZypTest(annual_data, byID = "Well_Num", col = "mean_GWL")
+## ---- message=FALSE, warning=FALSE---------------------------------------
+trends <- gwl_zyp_test(annual_data, byID = "Well_Num", col = "mean_GWL")
 trends
 
-## ---- warning=FALSE------------------------------------------------------
-trend_plot <- gwlAreaPlot(full_monthly_data, trend = trends$trend[1],
-                          intercept = trends$intercept[1], sig = trends$sig[1],
-                          state = "Stable", mkperiod = "annual", showInterpolated = TRUE)
+## ---- message=FALSE, warning=FALSE---------------------------------------
+result <-  trends %>% 
+  filter(test_type == "yuepilon") %>%
+  mutate(state = case_when(trend >= 0.1 & sig < 0.05 ~ "Large Rate of Decline",
+                           trend >= 0.03 & trend < 0.1 & sig < 0.05 ~ "Moderate Rate of Decline",
+                           trend <= -0.03 & sig < 0.05 ~ "Increasing",
+                           TRUE ~ "Stable")) 
+result
+
+## ---- message=FALSE, warning=FALSE, fig.align='center'-------------------
+trend_plot <- gwl_area_plot(full_monthly_data, trend = result$trend,
+                          intercept = result$intercept, sig = result$sig, state = result$state,
+                          mkperiod = "annual", showInterpolated = TRUE)
 plot(trend_plot)
 
 
