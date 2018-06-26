bd7a56c409fd3ed1c021fa4ced91fc5d742cfa26
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2018-06-26 15:54:40 -0700

    simplify show_stable_line logic
    Co-authored-by: Andy Teucher <andy.teucher@gov.bc.ca>

--- R/gwlAreaPlot.R
+++ R/gwlAreaPlot.R
@@ -129,23 +129,14 @@ gwl_area_plot <- function(dataframe, trend, intercept, trend_category,
     override_list <- lapply(override_list, `[`, 1)
   }
   
-  if (show_stable_line) {
+  if (show_stable_line || tolower(trend_category) != "stable") {
     plot.area <- plot.area + 
       geom_abline(aes_string(intercept = "intercept", slope = "slope", colour = "'LTT'"),
-                  data = data.frame(intercept = -int.well, slope = slope), size = 1) 
-  } else {
-    if (tolower(trend_category) == "stable") {
-      plot.area
-      
-      vals <- vals[2]
-      labs <- labs[2]
-      override_list <- lapply(override_list, `[`, 2)
-      
-    } else {
-      plot.area <- plot.area + 
-        geom_abline(aes_string(intercept = "intercept", slope = "slope", colour = "'LTT'"),
-                    data = data.frame(intercept = -int.well, slope = slope), size = 1) 
-    }
+                  data = data.frame(intercept = -int.well, slope = slope), size = 1)
+  } else if (tolower(trend_category) == "stable") {
+    vals <- vals[2]
+    labs <- labs[2]
+    override_list <- lapply(override_list, `[`, 2)
   }
   
   plot.area <- plot.area +
